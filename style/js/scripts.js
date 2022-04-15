7722027535dbe2dccb9c06c6ad6055969af90ac4
Andrew Davies <95408751+andrewd2021@users.noreply.github.com>
2022-04-15 11:12:59 +0100

    Add files via upload

---
+++ style/js/scripts.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+    'use strict';
+    /*-----------------------------------------------------------------------------------*/
+    /*	STICKY HEADER
+    /*-----------------------------------------------------------------------------------*/
+    var options = {
+        offset: 350,
+        offsetSide: 'top',
+        classes: {
+            clone: 'banner--clone fixed',
+            stick: 'banner--stick',
+            unstick: 'banner--unstick'
+        },
+        onStick: function() {
+            $($.SmartMenus.Bootstrap.init);
+        }
+    };
+    var banner = new Headhesive('.navbar', options);
+    /*-----------------------------------------------------------------------------------*/
+    /*	HAMBURGER MENU ICON
+    /*-----------------------------------------------------------------------------------*/
+    $(".nav-bars").on( "click", function() {
+        $(".nav-bars").toggleClass("is-active");
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	IMAGE ICON HOVER
+    /*-----------------------------------------------------------------------------------*/
+    $('.overlay a').prepend('<span class="over"><span></span></span>');
+    /*-----------------------------------------------------------------------------------*/
+    /*	TAB COLLAPSE
+    /*-----------------------------------------------------------------------------------*/
+    $('#tab1').tabCollapse({
+        tabsClass: 'hidden-sm hidden-xs',
+        accordionClass: 'visible-sm visible-xs'
+    });
+    $('#tab1').on('shown-accordion.bs.tabcollapse', function() {
+        $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+        $('.panel-group').on('shown.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').addClass(' panel-active');
+        }).on('hidden.bs.collapse', function(e) {
+            $(e.target).closest('.panel-default').removeClass(' panel-active');
+        });
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOGGLE
+    /*-----------------------------------------------------------------------------------*/
+    $('.panel-group').find('.panel-default:has(".in")').addClass('panel-active');
+    $('.panel-group').on('shown.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').addClass(' panel-active');
+    }).on('hidden.bs.collapse', function(e) {
+        $(e.target).closest('.panel-default').removeClass(' panel-active');
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	DATA REL
+    /*-----------------------------------------------------------------------------------*/
+    $('a[data-rel]').each(function() {
+        $(this).attr('rel', $(this).data('rel'));
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	TOOLTIP
+    /*-----------------------------------------------------------------------------------*/
+    if ($("[rel=tooltip]").length) {
+        $("[rel=tooltip]").tooltip();
+    }
+    /*-----------------------------------------------------------------------------------*/
+    /*	PRETTIFY
+    /*-----------------------------------------------------------------------------------*/
+    window.prettyPrint && prettyPrint();
+    /*-----------------------------------------------------------------------------------*/
+    /*	LOADER
+    /*-----------------------------------------------------------------------------------*/
+    $(".pageloader").lsPreloader({
+
+        backgroundColor: "#FFF",
+        logoImage: "style/images/logo.png",
+        minimumTime: .5,
+        progressHeight: "0",
+        progressColor: "#404040",
+        percentFontSize: "18px"
+
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	VANILLA
+    /*-----------------------------------------------------------------------------------*/
+    var myForm;
+    myForm = new VanillaForm($("form.vanilla-form"));
+	/*-----------------------------------------------------------------------------------*/
+	/* WIDTH CLASS
+	/*-----------------------------------------------------------------------------------*/
+	checkWidth(true); 
+	function checkWidth(init)
+	{
+	    if ($(window).width() < 1271) {
+	        $('body').addClass('mode-md');
+	    }
+	    else {
+	        if (!init) {
+	            $('body').removeClass('mode-md');
+	        }
+	    }
+	}
+	$(window).resize(function() {
+	    checkWidth(false);
+	});
+	
+    /*-----------------------------------------------------------------------------------*/
+    /*	COUNTER UP
+    /*-----------------------------------------------------------------------------------*/
+    $('.fcounter').counterUp({
+        delay: 50,
+        time: 1000
+    });
+    /*-----------------------------------------------------------------------------------*/
+    /*	COLLAGEPLUS
+    /*-----------------------------------------------------------------------------------*/
+
+});
+	
